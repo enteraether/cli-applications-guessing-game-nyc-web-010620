a459ca64865f30201eebf0fb5f3e727ba0cb2de0
Theresa <thetheresagarcia@gmail.com>
2020-01-02 01:33:36 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,7 @@
 def run_guessing_game
   comp_gen_rndm_num = rand(1..6)
   puts "Enter a number between 1 and 6 please"
-  gets.chomp = user_num
+  user_num = gets.chomp
   if user_num == comp_gen_rndm_num
     puts "You guessed the correct number!"
   elsif user_num != comp_gen_rndm_num
